17f8d73f5894817da6590c252340207eaa8d069d
pmularczyk <p.mularczyk@protonmail.com>
2020-07-05 23:30:48 +0200

    feat(devops): moved files to app for deployment to linode

--- store/store/settings.py
+++ store/store/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '45.79.251.214',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -120,7 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
