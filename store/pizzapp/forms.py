c36e99fbcef04087d6aca49dd710a96150f79ed4
pmularczyk <p.mularczyk@protonmail.com>
2020-07-05 12:14:40 +0200

    feat(forms): removed contactform and added readonly attributes to orderform

--- store/pizzapp/forms.py
+++ store/pizzapp/forms.py
@@ -1,16 +1,13 @@
 from django import forms
 from .models import Order
 
-class ContactForm(forms.Form):
-    name = forms.CharField(label='Name', max_length=100)
-    email = forms.EmailField(label='Email')
-    phone = forms.IntegerField(required=False)
-    date = forms.DateField(label='Datum', widget=forms.TextInput(attrs={'type': 'date'}))
-    time = forms.TimeField(label='Uhrzeit', widget=forms.TextInput(attrs={'type': 'time'}))
-    guests = forms.IntegerField(label='Personenanzahl',min_value=1, max_value=100)
-    message = forms.CharField(label='Ihre Nachricht', widget=forms.Textarea, required=False)
-
 class OrderForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       self.fields['pizza'].widget.attrs['readonly'] = True
+       self.fields['size'].widget.attrs['readonly'] = True
+
     class Meta:
         model = Order
-        fields = ('name', 'email', 'phone_number',)
+        fields = ('name', 'email', 'phone_number', 'pizza', 'size')
